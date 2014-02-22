a734dc718bef3435a8dc2184ff7379fc1eb9a1bc
Snuzzo <jrracinfan@gmail.com>
2014-02-22 14:30:48 -0500

    Moved mka eggs build script to vendor
    Change-Id: I5b0637a6aa82607fc02afa5dd8e1097b553236e8

--- envsetup.sh
+++ envsetup.sh
@@ -1,4 +1,4 @@
-illusionfunction hmm() {
+function hmm() {
 cat <<EOF
 Invoke ". build/envsetup.sh" from your shell to add the following functions to your environment:
 - lunch:   lunch <product_name>-<build_variant>
