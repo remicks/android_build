1c607a6fa20067c09ffc3cf56147bc8c4ee56367
Snuzzo <jrracinfan@gmail.com>
2015-02-01 14:47:00 -0500

    1/2 Restore plain tweak config files at time of flash

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -177,6 +177,9 @@ class EdifyGenerator(object):
   def ExtractPlainTweakAroma(self):
     self.script.append('package_extract_file("system/addon.d/UPDATE-aromainstaller.zip", "/sdcard/Plain-Tweak-AromaInstaller.zip");')
 
+  def RestorePlainTweak(self):
+	self.script.append('run_program("/system/bin/plaintweak", "restoreplaintweakconfig");')
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
